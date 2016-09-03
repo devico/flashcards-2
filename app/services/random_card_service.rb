cc6fc30bb668dd772b925e5f0ff50dd605c9181d
Alexander Zutikov <zvlex.dev@gmail.com>
2016-09-03 16:40:58 +0400

    Rename method

--- app/services/random_card_service.rb
+++ app/services/random_card_service.rb
@@ -6,16 +6,16 @@ class RandomCardService
     @card_id = card_id
   end
 
-  def get_random_card
-   current_cards = user.current_block.try(:cards) || user.cards
-   current_cards.first_pending || current_cards.first_repeating
+  def fetch_random_card
+    current_cards = user.current_block.try(:cards) || user.cards
+    current_cards.first_pending || current_cards.first_repeating
   end
 
   def call
     if card_id
       user.cards.find(card_id)
     else
-      get_random_card
+      fetch_random_card
     end
   end
 end
