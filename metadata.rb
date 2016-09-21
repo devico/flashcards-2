cf79a55a561a112a5ed2f9be0d74d265b50ade0e
Alexander Zutikov <zvlex.dev@gmail.com>
2016-09-21 13:39:33 +0400

    Vagrant step 2

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'flc'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures flc'
+long_description 'Installs/Configures flc'
+version '0.1.0'
+
+depends 'rbenv'
